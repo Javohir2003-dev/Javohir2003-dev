9c3ac2c51eb532e396c82584e6f8899bc6ee3cee
Javohir <norbekovj61.com>
2024-10-04 14:33:09 +0500

    10-kommit

--- mein/urls.py
+++ mein/urls.py
@@ -7,7 +7,7 @@ from . import views as a
 urlpatterns = [
    path('create/', a.UserCreateListView.as_view(), name='user_create'),
    path('detail/<int:id>/', a.UserDetailView.as_view(),name='detail_user'),
-   path('category/', a.CategoryListView.as_view(),name='category'),
+   path('category/', a.CategoryAPIView.as_view(),name='category'),
    path('product/', a.ProductListView.as_view(),name='product'),
    path('product_image/', a.Product_image_ListView.as_view(),name='product_image'),
    path('signup/', a.Register.as_view()),
